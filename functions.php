8f8bc6bc2972b94c119f6a6be819ecae9c1fd9ec
patrosk <naiade99@gmail.com>
2021-10-20 14:07:03 +0200

    added background image and classes

--- functions.php
+++ functions.php
@@ -6,18 +6,20 @@ function getMonsters($array)
 {
     foreach ($array as $monster) : ?>
         <div>
-            <img src="<?php echo $monster['image'] ?>">
-            <h3><?php echo $monster['name'] . "\n" ?></h3>
+            <div class="name">
+                <img src="<?php echo $monster['image'] ?>">
+                <h3><?php echo $monster['name'] . "\n" ?></h3>
+            </div>
         </div>
-        <div>
-            <div>
+        <div class="info">
+            <div class="stats">
                 <div>Location: <?php echo $monster['info']['location'] ?></div>
                 <div>Creature: <?php echo $monster['info']['creature'] ?></div>
                 <div>Powers: <?php echo $monster['info']['powers'] ?></div>
                 <div>Weakness: <?php echo $monster['info']['weakness'] ?></div>
                 <div>Slain By: <?php echo $monster['info']['slain by'] ?></div>
             </div>
-            <div>
+            <div class="story">
                 <p>INSERT INFO/ STORY HERE</p>
             </div>
         </div>
