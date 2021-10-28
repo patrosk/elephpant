a1e48d8df4fadd5dd0001ed0a41408eb87336c36
patrosk <naiade99@gmail.com>
2021-10-28 22:16:35 +0200

    Added stories array

--- randomstory.php
+++ randomstory.php
@@ -3,5 +3,50 @@
 declare(strict_types=1);
 
 $stories = [
-    'asdflkj',
+    [
+        'Title' => 'The kidnapping of Persephone',
+        'Story' => "Perspehone was the beautiful daughter of Demeter, the goddess of harvest, grain, fertility
+    and all living plants. One day, while she was picking flowers on the fields of Sicily,
+    Hades, the god of the Underworld, appeared in his chariot and kidnapped her. He took her to
+    the Underworld where she was kept prisoner for sseveral years. Her mother Demeter was devastated that
+    her daughter had disappeared, and spent years searching for her. She was so busy with worrying about
+    her daughter, that she forgot about her duties as goddess - the earth dried out, harvests were bad, entire
+    fields of grain rotted. The people of Greece were starving. Finally, Demeter found out where her daughter
+    had been taken. She demanded that Hades return her, and he agreed but for one condition: Persephone must
+    have spent her time in the Underworld without touching any of the food there. The food in the Underworld is
+    the food of the dead, and whoever eats it must spend eternity there. Perspehone said she had never touched
+    the food of the dead and she was returned to her mother. Demeter was so happy that all plants quickened, fields
+    were filled with grain and sweet fruits hung from the trees. Hades was furious, he wanted Persephone as his queen.
+    He interrogated every soul in the Underworld until he found one who testified that Perspehone, in her desperation
+    to quench her thirst, had eaten seven seeds of a pomegranate. Triumphant, Hades demanded the return of Persephone.
+    Realising there was no other way out, Demeter agreed to part from her daughter, but threatened to neglect her
+    duties as a goddess. A compromise was reached: Persephone would spend nine months on Earth with her mother,
+    and three months in the Underworld. When she was with her mother, nature was blessed with life, the soil was rich
+    and harvests were plentiful. When she left for the Underworld, the trees lost their leaves, plants withered and the
+    earth froze. Nature was lifeless, until Persephone's return. When she left the Underworld, she brought spring with her.
+    This is how the seasons were created."
+    ],
+
+    [
+        'Title' => 'Zeus is a megalomaniac and a sex addict',
+        'Story' => "After overthrowing the cruel titans and instating himself and his siblings as the new gods, Zeus was
+        the most powerful god on Mount Olympus. He could pretty much do as he pleased, although the other gods sometimes
+        not only questioned him but also actively worked against him. He was the one to create humankind; the first humans lived
+        worry-free lives in sunshine and happiness until they died of old age. Next, Zeus created humans who unfortunately turned
+        out to ignore the gods completely. They were arrogant and complacent and constatntly arguing with each other, which
+        eventually made Zeus exterminate them all. Zeus created another batch of humans - these were smarter and gifted but
+        instead of using it for good, they spent all their time and capacities to design and manufacture weapons, with which
+        they then used to kill each other. Eventually, all of them were dead and spent eternity in the Underworld. Zeus made one
+        last attempt at creating humans and this time, he was succesful. He created the Age of Heroes, with numerous extraordinary
+        humans that would come to be praised in the ages to come. Like the other gods on Olympus, one of Zeus' favourite past-times
+        was visiting the mortal humans. His object was often to help those in need, but his meddling did not always end up
+        improving the humans' lives. Zeus and his wife Hera were an odd couple, almost constantly bickering and arguing. One constant
+        source of quarrel was Zeus permanent infidelity. Many are the stories where he visits beautiful mortal women, courts them, and if
+        they aren't charmed by him, he transforms himself into various creatures to win them over. To gain Hera's affection,
+        he turned himself into a sickly cuckoo, which she nursed back to health.
+
+
+
+         "
+    ]
 ];
